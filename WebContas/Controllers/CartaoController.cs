7949e0343f315aff40e34ff4a2b05e3f1de16e6c
Dell <Dell@DESKTOP-TK7GSO1>
2023-05-02 22:20:12 -0300

    Adicionar arquivos de projeto.

---
+++ WebContas/Controllers/CartaoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebContas.Controllers
+{
+    public class CartaoController : Controller
+    {
+        public CartaoController()
+        {
+            
+        }
+
+        public IActionResult GetCartoes()
+        {
+            return View();
+        }
+    }
+}
